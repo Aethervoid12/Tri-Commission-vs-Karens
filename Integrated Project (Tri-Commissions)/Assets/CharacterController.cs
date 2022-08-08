38c8e6b6d34c6b4089cd24bd4dc7bbed5625c8c8
Lee Jun Fai <92961436+Aethervoid12@users.noreply.github.com>
2022-08-08 15:50:44 +0800

    Isometric Movement

--- Integrated Project (Tri-Commissions)/Assets/CharacterController.cs
+++ Integrated Project (Tri-Commissions)/Assets/CharacterController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    private Vector3 _input;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GatherInput();
+        Look();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Look()
+    {
+        if (_input != Vector3.zero)
+        {
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0));
+            var skewedInput = matrix.MultiplyPoint3x4(_input);
+            var relative = (transform.position + skewedInput) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+        }
+    }
+
+    void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+    }
+
+    void Move()
+    {
+        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
     }
 }
