55d319d2a15746e862efa37ac6b0bbf297d173db
radelle <s10219371@connect.np.edu.sg>
2022-08-13 15:31:09 +0800

    Revert "Highlighting completed"
    This reverts commit b75024485afc165e0b9de7c723d9808d25012811.

--- Integrated Project (Tri-Commissions)/Assets/Scripts/Collectible.cs
+++ Integrated Project (Tri-Commissions)/Assets/Scripts/Collectible.cs
@@ -4,26 +4,5 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    private Outline outline;
-    public MeshRenderer myRenderer;
-    public Color[] newColors;
-    private int colorIndex = 0;
-    public Color startColor;
-
-    void OnTriggerEnter(Collider other)
-    {
-
-        if (other.gameObject.tag == "Player")
-        {
-            myRenderer.material.color = newColors[colorIndex];
-            
-        }
-        
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        myRenderer.material.color = startColor;
-    }
 
 }
