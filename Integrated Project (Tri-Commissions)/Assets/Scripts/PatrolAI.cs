55923afb2ee3c3a3fcfc353813b1ec8f4fafd37a
radelle <s10219371@connect.np.edu.sg>
2022-08-15 15:13:28 +0800

    Update PatrolAI.cs

--- Integrated Project (Tri-Commissions)/Assets/Scripts/PatrolAI.cs
+++ Integrated Project (Tri-Commissions)/Assets/Scripts/PatrolAI.cs
@@ -104,9 +104,10 @@ public class PatrolAI : MonoBehaviour
             yield return null;
             animator.SetBool("isWalking", true);
             animator.SetBool("isRunning", false);
+            Debug.Log("Walking");
             //Debug.Log(currentAnim);
-            
-            
+
+
             if (!hasReached)
             {
                 if(agent.remainingDistance <= agent.stoppingDistance)
