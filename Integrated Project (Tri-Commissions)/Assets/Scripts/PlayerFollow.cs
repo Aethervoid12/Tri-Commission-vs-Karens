052cfa82dc11e5b03e67372558bc8117ceaf4936
Ng shijun Jess <S10222791@connect.np.edu.sg>
2022-08-16 17:24:28 +0800

    addded XML comments for Player follow

--- Integrated Project (Tri-Commissions)/Assets/Scripts/PlayerFollow.cs
+++ Integrated Project (Tri-Commissions)/Assets/Scripts/PlayerFollow.cs
@@ -8,17 +8,19 @@ public class PlayerFollow : MonoBehaviour
 
     private Vector3 _cameraOffset;
 
-    //
+    //Smoothness of the camera to follow the player
     [Range(0.01f,1.0f)]
     public float SmoothFactor = 0.5f;
 
     // Start is called before the first frame update
+    // Camera's distance from the player
     void Start()
     {
         _cameraOffset = transform.position - PlayerTransform.position;
     }
 
     // Update is called once per frame
+    // When the player moves, the camera will go to the new position
     void LateUpdate()
     {
         Vector3 newPos = PlayerTransform.position + _cameraOffset;
